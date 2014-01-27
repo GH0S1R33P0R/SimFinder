fff6f9731fcb23a509bf83f4046145cd8044268d
GH0S1_R33P0R <gh0s1@jubatus>
2014-01-26 19:47:11 -0500

    Successfully obtained NCD result

--- ncdPrototype.py
+++ ncdPrototype.py
@@ -13,11 +13,15 @@ def compressionSize(stringIn):
         Output:
             length of the string after it is compressed
     """
+
     #TODO (Bader): take in binary input from file
+
     bytesIn = bytes(stringIn, 'utf-8')
+
     compressed = zlib.compress(bytesIn)
     length = len(compressed)
-    print("Length is: "  + str(length))
+#    print("Arg in is: [" + stringIn + "] with length: " + str(length))
+    return length
 
 
 def main():
@@ -27,10 +31,20 @@ def main():
     str1 = input("Enter the first string:")
     str2 = input("Enter the second string:")
 
-    print("The first string is: " + str1)
-    print("The second string is: " + str2)
+#   print("The first string is: " + str1)
+#   print("The second string is: " + str2)
 
-    compressionSize(str1)
+    #TODO (Bader): Make this into a function
+    NCD_A = compressionSize(str1+str2)
+    print("NCD_A: " + str(NCD_A))
+    NCD_B = min(compressionSize(str1), compressionSize(str2))
+    print("NCD_B: " + str(NCD_B))
+    NCD_C = max(compressionSize(str1), compressionSize(str2))
+    print("NCD_C: " + str(NCD_C))
+
+    NCD_result = (NCD_A - NCD_B) / NCD_C
+
+    print("The NCD result is: " + str(NCD_result)) 
 
 if __name__ == '__main__':
     main()

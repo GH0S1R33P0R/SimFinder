fe09d706c21055a9d117dfa23134f2c1b0874368
GH0S1_R33P0R <gh0s1@jubatus>
2014-01-26 20:03:07 -0500

    Removed debugging print statements

--- ncdPrototype.py
+++ ncdPrototype.py
@@ -15,12 +15,10 @@ def compressionSize(stringIn):
     """
 
     #TODO (Bader): take in binary input from file
-
     bytesIn = bytes(stringIn, 'utf-8')
 
     compressed = zlib.compress(bytesIn)
     length = len(compressed)
-#    print("Arg in is: [" + stringIn + "] with length: " + str(length))
     return length
 
 def getNCD(str1, str2):
@@ -31,18 +29,12 @@ def getNCD(str1, str2):
         Output:
             int: The distance according to NCD
     """
-
     NCD_A = compressionSize(str1+str2)
-    print("NCD_A: " + str(NCD_A))
     NCD_B = min(compressionSize(str1), compressionSize(str2))
-    print("NCD_B: " + str(NCD_B))
     NCD_C = max(compressionSize(str1), compressionSize(str2))
-    print("NCD_C: " + str(NCD_C))
 
     NCD_result = (NCD_A - NCD_B) / NCD_C #This is the formula that is used
-    print("The NCD result is: " + str(NCD_result)) 
     return NCD_result
-
 
 def main():
     str1 = ''
@@ -51,8 +43,6 @@ def main():
     str1 = input("Enter the first string:")
     str2 = input("Enter the second string:")
 
-#   print("The first string is: " + str1)
-#   print("The second string is: " + str2)
     distance = getNCD(str1, str2)
     print("Normalize compression distance is: " + str(distance))
 

48979014db8f8a7e26da68bc0c7a507df5badc69
GH0S1_R33P0R <gh0s1@jubatus>
2014-03-11 02:14:44 -0400

    Added append method for ICompressible

--- SeniorProject/SeniorProject/ICompressible.cs
+++ SeniorProject/SeniorProject/ICompressible.cs
@@ -8,5 +8,12 @@ namespace SeniorProject
         /// </summary>
         /// <returns>Returns a byte[] representation of the entity.</returns>
         byte[] ToByteArray();
+
+        /// <summary>
+        /// Returns a new ICompressible that contains the first ICompressible followed by the second.
+        /// </summary>
+        /// <param name="input">ICompressible to be appended.</param>
+        /// <returns>The resulting ICompressible.</returns>
+        ICompressible append(ICompressible input );
     }
 }

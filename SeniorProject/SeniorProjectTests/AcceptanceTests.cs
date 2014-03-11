1c44d3d4a28ea06abe354e8c126c2141666ddbfc
Edward Riley <edward.riley.jr@gmail.com>
2014-03-11 14:16:46 -0400

    Added a class for Acceptance Testing
    Initial creation of acceptance testing class in testing project

---
+++ SeniorProject/SeniorProjectTests/AcceptanceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeniorProject;
+
+namespace SeniorProjectTests
+{
+    public class AcceptanceTests
+    {
+        public bool ContainsMatch(ICompressible testTicket, ICompressible potentialMatch, ICompressible[] dataSet)
+        {
+            //Similarity object to use for FindSimilarEntities
+            Similarity simTest = new Similarity(); 
+
+            //Get the order results and return if they match is present
+            ICompressible[] results = simTest.FindSimilarEntities(testTicket, dataSet);
+
+            if(results.Contains(potentialMatch))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

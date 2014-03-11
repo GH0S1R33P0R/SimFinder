68a0b6612fbfe36051d22db0d57e51353d091b2e
GH0S1_R33P0R <gh0s1@jubatus>
2014-03-11 03:44:54 -0400

    Implemented MockEntity

--- SeniorProject/SeniorProjectTests/MockEntity.cs
+++ SeniorProject/SeniorProjectTests/MockEntity.cs
@@ -4,5 +4,24 @@ namespace SeniorProjectTests
 {
     class MockEntity: ICompressible
     {
+        private byte[] data;
+
+        MockEntity(byte[] data)
+        {
+            this.data = data;
+        }
+
+        byte[] ICompressible.ToByteArray()
+        {
+            byte [] toReturn;
+
+            toReturn = new byte [3] {1,2,3};
+            return toReturn;
+        }
+
+        void ICompressible.setData(byte[] input)
+        {
+            this.data = input;
+        }
     }
 }

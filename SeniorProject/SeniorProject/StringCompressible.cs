8eb232e56b653e27dd76f5f9899a2c0b70aa103c
Edward Riley <edward.riley.jr@gmail.com>
2014-03-11 12:47:20 -0400

    Added StringCompressible class
    This implements ICompressible and has a constructor which takes a string

---
+++ SeniorProject/SeniorProject/StringCompressible.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorProject
+{
+    public class StringCompressible : ICompressible
+    {
+        private double complexity;
+        private byte[] data;
+
+        //Default Constructor
+        public StringCompressible()
+        {
+            complexity = 0;
+            data = null;
+        }
+       
+        //Constructor that takes string data
+        public StringCompressible(string data)
+        {
+            this.data = Encoding.ASCII.GetBytes(data);
+        }
+
+        byte[] ICompressible.ToByteArray()
+        {
+            return data;
+        }
+
+        double ICompressible.Complexity
+        {
+            get { return this.complexity; }
+            set { this.complexity = value; }
+        }
+    }
+}

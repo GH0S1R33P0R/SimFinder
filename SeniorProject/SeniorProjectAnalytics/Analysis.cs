632ad9a89852c1c44a1f4731354c6683df17cb85
GH0S1_R33P0R <gh0s1@margarita>
2014-03-13 18:18:53 -0400

    Added unimplemented Analysis class
    The  Analysis class will take a list of tickets from a file and then
    proceed to output a file of each tickets ID along with a list of each
    similar ticket to it.
    
    TODO notes are placed for what needs to be coded.

---
+++ SeniorProject/SeniorProjectAnalytics/Analysis.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeniorProject;
+
+namespace SeniorProjectAnalytics
+{
+    class Analysis
+    {
+        private ISimilarity isSim;
+        private ICompressible[] inputs;
+        private ICompressible[] output;
+
+        /// <summary>
+        /// Read file "input.txt" and set list of TicketCompressible items.
+        /// </summary>
+        /// <param name="entitys">Array of Ticket to be overwritten.</param>
+        public void ObtainTickets(ref TicketCompressible[] entitys)
+        {
+            // TODO: implement
+        }
+
+        /// <summary>
+        /// Finds all tickets which are similar to the entity. 
+        /// </summary>
+        /// <param name="entity">Ticket to use for comparison.</param>
+        /// <param name="entityList">Array of tickets to compare against.</param>
+        /// <returns>An array of tickets similar to entity.</returns>
+        public TicketCompressible[] FindSimilars(TicketCompressible entity, TicketCompressible[] entityList)
+        {
+            // TODO: implement
+            return entityList;
+        }
+
+        public void Main(string[] args )
+        {
+            // TODO: implement
+
+            // TODO: ObtainTickets
+
+            // TODO: FindSimilars for each ticket and write to file
+        }
+    }
+}

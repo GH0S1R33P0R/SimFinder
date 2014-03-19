8cab27077e8bac87a6881c466b826bfc60ad439c
Edward Riley <edward.riley.jr@gmail.com>
2014-03-18 23:06:38 -0400

    Added REPL to driver; removed debug code

--- SeniorProject/SeniorProjectAnalytics/Program.cs
+++ SeniorProject/SeniorProjectAnalytics/Program.cs
@@ -77,37 +77,47 @@ namespace SeniorProjectAnalytics
                 //Console.WriteLine("itemID: {0}, summary: {1}", row.Substring(0, row.IndexOf(',')), row.Substring(row.IndexOf(',')+1));
             }
 
-            //Get the item ID to search for and store it in searchID
-            Console.WriteLine("Enter the itemID of the ticket you would like to find similarities for: ");
-            searchID = Console.ReadLine();
-
-            //Start the timer before searching
-            timer = Stopwatch.StartNew();
-
-            foreach (StringCompressible ticket in DataSet)
+            while (true)
             {
-                if (ticket.ItemID.Equals(searchID))
-                {
-                    results = simObject.FindSimilarValAndEntities(ticket, DataSet.ToArray());
-                }
-            }
+                //Get the item ID to search for and store it in searchID
+                Console.WriteLine("Enter 'q' to exit.");
+                Console.WriteLine("Enter the itemID of the ticket you would like to find similarities for: ");
+                searchID = Console.ReadLine();
 
-            int counter = 1;
-
-            foreach (Tuple<double, StringCompressible> ticket in results)
-            {
-                if (counter > 20)
+                //REPL control
+                if (searchID.Equals("q"))
                 {
                     break;
                 }
-                Console.WriteLine("{0}.\tTicket ID: {1}\tConfidence Rating: {2}", counter, ticket.Item2.ItemID, ticket.Item1);
-                counter++;
-            }
 
-            timer.Stop();
-            Console.WriteLine();
-            Console.WriteLine("Searched 5,000 tickets and produced results in {0} ms", timer.ElapsedMilliseconds);
-            Console.ReadLine();
+                //Start the timer before searching
+                timer = Stopwatch.StartNew();
+
+                foreach (StringCompressible ticket in DataSet)
+                {
+                    if (ticket.ItemID.Equals(searchID))
+                    {
+                        results = simObject.FindSimilarValAndEntities(ticket, DataSet.ToArray());
+                    }
+                }
+
+                int counter = 1;
+
+                foreach (Tuple<double, StringCompressible> ticket in results)
+                {
+                    if (counter > 20)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("{0}.\tTicket ID: {1}\tConfidence Rating: {2}", counter, ticket.Item2.ItemID, ticket.Item1);
+                    counter++;
+                }
+
+                timer.Stop();
+                Console.WriteLine();
+                Console.WriteLine("Searched 5,000 tickets and produced results in {0} ms", timer.ElapsedMilliseconds);
+                Console.WriteLine();
+            }
         }
     }
 }
